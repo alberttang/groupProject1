d7b01a6dd8cf14ac20ebb317a085d4baa1c16814
elmarc <elmarc@USER2017s-MacBook-Pro.local>
2017-11-04 18:41:21 -0700

    added google autocomplete, generated latitude and longitude, and added required IDs to the search box on index page

--- app2.js
+++ app2.js
@@ -1,37 +1,54 @@
-var pyrmont = new google.maps.LatLng(33.6845673, -117.82650490000003);
+function initialize() {
+    var input = document.getElementById('city-input');
+    var autocomplete = new google.maps.places.Autocomplete(input);
+    google.maps.event.addListener(autocomplete, 'place_changed', function() {
+        var place = autocomplete.getPlace();
+        if (!place.geometry) {
+            return;
+        }
+        var lat = place.geometry.location.lat();
+        var lng = place.geometry.location.lng();
+        console.log(place.geometry.location.lat());
+        console.log(place.geometry.location.lng());
 
-//********** "Panel_Image" needs be replaced with an id on the div holding the results **********\\
+        //place.name;
+        var pyrmont = new google.maps.LatLng(lat, lng);
 
-var map = new google.maps.Map(document.getElementById("Panel_Image"), {
-    center: pyrmont,
-    zoom: 15
-});
-var request = {
-    location: pyrmont,
-    radius: '5000',
-    type: ['restaurant']
-};
+        //********** "Panel_Image" needs be replaced with an id on the div holding the results **********\\
 
-service = new google.maps.places.PlacesService(map);
-service.nearbySearch(request, function(resp) {
-    //console log response
-    console.log(resp);
-    console.log("Restaurant Name:" + " " + resp[0].name);
-    console.log("Address:" + " " + resp[0].vicinity);
-    console.log("Opening Hours:" + " " + resp[0].opening_hours.open_now);
-    console.log("Raiting:" + " " + resp[0].rating);
-    console.log("Restaurant Photo URL:" + " " + resp[0].photos[0].getUrl({ maxWidth: 400 }));
+        var map = new google.maps.Map(document.getElementById("Panel_Image"), {
+            center: pyrmont,
+            zoom: 15
+        });
+        var request = {
+            location: pyrmont,
+            radius: '5000',
+            type: ['restaurant']
+        };
 
-    // Push to HTML
-    $("#restaurantName").html(resp[0].name);
-    $("#restaurantAddress").html(resp[0].vicinity);
-    $("#restaurantHours").html(resp[0].opening_hours.open_now);
-    $("#restaurantRating").html(resp[0].rating);
-    // generate image
-    var photoUrl = resp[0].photos[0].getUrl({ maxWidth: 400 });
-    var restaurantPhoto = $("<img>");
-    restaurantPhoto.attr("src", photoUrl);
-    restaurantPhoto.attr("alt", "cat image");
-    $("#restaurantPhoto").prepend(restaurantPhoto);
+        service = new google.maps.places.PlacesService(map);
+        service.nearbySearch(request, function(resp) {
+            //console log response
+            console.log(resp);
+            console.log("Restaurant Name:" + " " + resp[0].name);
+            console.log("Address:" + " " + resp[0].vicinity);
+            console.log("Opening Hours:" + " " + resp[0].opening_hours.open_now);
+            console.log("Raiting:" + " " + resp[0].rating);
+            console.log("Restaurant Photo URL:" + " " + resp[0].photos[0].getUrl({ maxWidth: 400 }));
 
-});
+            // Push to HTML
+            $("#restaurantName").html(resp[0].name);
+            $("#restaurantAddress").html(resp[0].vicinity);
+            $("#restaurantHours").html(resp[0].opening_hours.open_now);
+            $("#restaurantRating").html(resp[0].rating);
+            // generate image
+            var photoUrl = resp[0].photos[0].getUrl({ maxWidth: 400 });
+            var restaurantPhoto = $("<img>");
+            restaurantPhoto.attr("src", photoUrl);
+            restaurantPhoto.attr("alt", "cat image");
+            $("#restaurantPhoto").prepend(restaurantPhoto);
+
+        });
+    });
+}
+google.maps.event.addDomListener(window, 'load', initialize);
