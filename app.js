71bbd275a51803a395d490c10702b94d59c3543e
Rey Cruz <reycruz@dhcp-v082-094.mobile.uci.edu>
2017-11-07 19:50:06 -0800

    made food arrays into 3-4 items

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ var foodType
 
 // added Cold, Warm, Hot food variables - pre determined array of food types
 // array of food recommendations // rey
-    var cold = ["Ramen", "Pho", "Soup (Boudin/Soup Plantation)", "Shabu (hot pot)", "Korean BBQ", "Soon Do Boo (Korean Tofu Soup)", "Chicken Tikka Masala", "Hot chocolate"];
-    var warm = ["Boba", "Ice Cream", "Mac n cheese", "Shaved Ice", "Burgers (In and out)", "Café’s"];
-    var hot = ["Boba", "Shaved Ice ", "Gelato", "Salad", "Smoothies (Jamba Juice)", "Breweries (Anaheim Packing House)"];
+    var cold = ["Ramen", "Pho", "Soup",];
+    var warm = ["Boba", "Ice Cream",];
+    var hot = ["Boba", "Shaved Ice ", "Gelato", "Salad", "Smoothies",];
 
 // initialize function
 function initialize() {
