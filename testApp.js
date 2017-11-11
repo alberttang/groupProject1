f24e05f14df68b9fb4c73f3f3bddd878cb2a3f3e
Albert Tangtheinkul <bamf_albert@yahoo.com>
2017-11-10 20:14:14 -0800

    changed index and js on the New Test Files

--- testApp.js
+++ testApp.js
@@ -6,9 +6,31 @@ var coldFoods = ["Pho", "Burgers", "Pizza", "Soup", "Hot Chocolate"]
 var warmFoods = ["Boba", "Ice Cream", "Mac n Cheese", "Shaved Ice", "Burgers"]
 var hotFoods = ["Shaved Ice", "Boba", "Gelato", "Salad", "Smoothies", "Breweries"]
 
+//*************** Firebase **********************************************************************
+
+var config = {
+                apiKey: "AIzaSyBQaM8Oo_2iZEiV1AHFl3p1YCkAwOpLacA",
+                authDomain: "myproject-e1235.firebaseapp.com",
+                databaseURL: "https://myproject-e1235.firebaseio.com",
+                projectId: "myproject-e1235",
+                storageBucket: "myproject-e1235.appspot.com",
+                messagingSenderId: "402017814268"
+    };
+
+    firebase.initializeApp(config);
+
+
+    // Create a variable to reference the database
+
+    var database = firebase.database();
+
+//**************************************************************************************************
+
+
 function keypress() {
     $(document).on("keypress", "form", function(event) {
         return event.keyCode != 13;
+
     });
 }
 // initialize function
@@ -21,6 +43,7 @@ function initialize() {
     var autocomplete = new google.maps.places.Autocomplete(input);
     google.maps.event.addListener(autocomplete, 'place_changed', function() {
         var place = autocomplete.getPlace();
+        var inputLocation = place.formatted_address;
         if (!place.geometry) {
             return;
         }
@@ -41,7 +64,12 @@ function initialize() {
 
         // Func() for calling the Google Places API, for use of grabing the food type inside of the Weather AJAX http request
 
-        function googlePlacesSearch(food1,food2,food3, food4, food5) {
+        function googlePlacesSearch(food1, food2, food3, food4, food5) {
+
+            $("#start-input").addClass("hide");
+            $("#results").removeClass("hide");
+
+
             $(".firstCard").empty();
 
 
@@ -49,68 +77,84 @@ function initialize() {
             console.log('gps what food? ', foods);
 
             //request variable used to run the Google Places Search
-                for (var i = 0; i < 5; i++) {
+            for (var i = 0; i < 4; i++) {
 
-            var request = {
-                location: center,
-                radius: '5000',
-                keyword: foods[i]
+                var request = {
+                    location: center,
+                    radius: '5000',
+                    keyword: foods[i]
+                };
+
+                console.log("**********LOOOKKKKK " + foods[i])
+
+                // running the Google Places API Search 
+                service = new google.maps.places.PlacesService(map);
+                service.nearbySearch(request, function(resp) {
+
+                    console.log(resp);
+
+                    //console log response
+                    console.log("this is a test print" + foods)
+                    console.log("Restaurant Name:" + " " + resp[0].name);
+                    console.log("Address:" + " " + resp[0].vicinity);
+                    console.log("Rating:" + " " + resp[0].rating);
+                    console.log("Restaurant Photo URL:" + " " + resp[0].photos[0].getUrl({ maxWidth: 6000 }));
+
+                    // Push to HTML
+                    var resName = resp[0].name;
+                    $("#restaurantName").html(resp[0].name);
+                    var resAddress = resp[0].vicinity;
+                    $("#restaurantAddress").html(resp[0].vicinity);
+                    //$("#restaurantHours").html(resp[0].opening_hours.open_now);
+                    var resRating = resp[0].rating;
+                    $("#restaurantRating").html(resp[0].rating);
+                    // generate image
+                    var photoUrl = resp[0].photos[0].getUrl({ maxWidth: 6000 });
+
+                    console.log(photoUrl);
+
+                    if (typeof photoUrl === 'undefined' || !photoUrl || null ){
+                        photoURL = "via.placeholder.com/200x200"
+                    } 
+
+                    var restaurantPhoto = $("<img>");
+                    var insertImage = $("#insert-image");
+                    restaurantPhoto.attr("src", photoUrl);
+                    restaurantPhoto.attr("alt", resp[0].name);
+                    // $("#insert-image").append(restaurantPhoto);
+                    console.log("look at this element" + restaurantPhoto);
+
+
+                    // Creates a div to hold the movie
+                    var cardContainer = $("<div>");
+                    cardContainer.attr("class", "col s12 m6 hoverable")
+                    var cardDiv = $("<div>");
+                    cardDiv.attr("class", "card")
+                    var cardImageDiv = $("<div>");
+                    cardImageDiv.attr("class", "card-image");
+                    cardDiv.append(cardImageDiv);
+                    cardImageDiv.append(restaurantPhoto);
+                    var cardSpan = $("<span>");
+                    cardSpan.attr("class", "card-title");
+                    cardSpan.html(resName);
+                    var cardContent = $("<div>");
+                    cardContent.attr("class", "card-content");
+                    var cardP = $("<p>");
+                    cardP.html("<p>" + resAddress + "</p><p>Rating: " + resRating +"</p>")
+                    cardImageDiv.append(cardSpan);
+                    cardContent.append(cardP);
+                    cardDiv.append(cardContent)
+                    cardContainer.append(cardDiv)
+                    $(".firstCard").append(cardContainer);
+
+                                        // insertImage.html(restaurantPhoto);
+
+
+
+                });
+
             };
-
-            console.log("**********LOOOKKKKK " + foods[i])
-
-            // running the Google Places API Search 
-            service = new google.maps.places.PlacesService(map);
-            service.nearbySearch(request, function(resp) {
-
-                console.log(resp);
-
-                //console log response
-                console.log("this is a test print" + foods)
-                console.log("Restaurant Name:" + " " + resp[2].name);
-                console.log("Address:" + " " + resp[2].vicinity);
-                console.log("Rating:" + " " + resp[2].rating);
-                console.log("Restaurant Photo URL:" + " " + resp[0].photos[0].getUrl({ maxWidth: 100 }));
-
-                // Push to HTML
-                var resName = resp[0].name;
-                $("#restaurantName").html(resp[0].name);
-                var resAddress = resp[0].vicinity;
-                $("#restaurantAddress").html(resp[0].vicinity);
-                //$("#restaurantHours").html(resp[0].opening_hours.open_now);
-                var resRating = resp[0].rating;
-                $("#restaurantRating").html(resp[0].rating);
-                // generate image
-                var photoUrl = resp[0].photos[0].getUrl({ maxWidth: 400 });
-                var restaurantPhoto = $("<img>");
-                restaurantPhoto.attr("src", photoUrl);
-                restaurantPhoto.attr("alt", resp[0].name);
-                $("#restaurantPhoto").html(restaurantPhoto);
-
-
-                // Creates a div to hold the movie
-                var cardDiv = $("<div>");
-                cardDiv.attr("class", "card")
-                var cardImageDiv = $("<div>");
-                cardImageDiv.attr("class", "card-image");
-                cardDiv.append(cardImageDiv);
-                cardImageDiv.append(restaurantPhoto);
-                var cardSpan = $("<span>");
-                cardSpan.attr("class", "card-title");
-                cardSpan.html(resName);
-                var cardContent = $("<div>");
-                var cardP = $("<p>");
-                cardP.html("Adress: " + resAddress + "  -Rating: " + resRating)
-                cardContent.append(cardSpan);
-                cardContent.append(cardP);
-                cardDiv.append(cardContent)
-                $(".firstCard").append(cardDiv);
-
-
-            });
-
-        };
-    }
+        }
 
 
         //************** Weathe API Querying AJAX*******************************************
@@ -126,6 +170,12 @@ function initialize() {
 
             console.log(response);
             console.log("Temp: " + response.main.temp);
+            console.log(inputLocation);
+
+
+          
+
+
 
 
             //***********************************************************************************************************
@@ -136,6 +186,12 @@ function initialize() {
 
             //grab the temperature of the weathe API
             temperature = response.main.temp;
+
+             database.ref().set({
+            usrLocation: inputLocation,
+            usrTemp: temperature,
+            
+          });
 
             //if statement to determine if the temperature is hot or warm or cold
             if (temperature < 70) {
@@ -156,24 +212,40 @@ function initialize() {
 
 
             //***********If statment to determine what food to query *************************
-            if(foodType === "Cold"){
+            if (foodType === "Cold") {
 
                 foods = coldFoods;
 
-            } else  if (foodType === "Warm"){
+            } else if (foodType === "Warm") {
 
                 foods = warmFoods;
-            } else{
+            } else {
                 foods = hotFoods;
             }
             //*********************************************************************************
 
             //** Run Function to query Google Places API with Foodtype
-            googlePlacesSearch(foods[0],foods[1],foods[2],foods[3],foods[4]);
+            googlePlacesSearch(foods[0], foods[1], foods[2], foods[3], foods[4]);
 
         });
     });
 }
+
+//************FIREBASE*****************************************************************
+
+database.ref().on("value", function(snapshot) {
+
+        console.log(snapshot.val());
+
+
+      $("#recent-display").html( "<p> Location: " + snapshot.val().usrLocation + "</p><p> Temperature: " + snapshot.val().usrTemp + "°F");
+     
+
+    }, function(errorObject) {
+
+        console.log("The read failed: " + errorObject.code);
+    });
+//******************************************************************************************
 
 
 google.maps.event.addDomListener(window, 'load', initialize);
