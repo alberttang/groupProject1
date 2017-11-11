b81fd2f7e5e56c759e97d2abbc866469992724d9
Albert Tangtheinkul <bamf_albert@yahoo.com>
2017-11-10 22:16:22 -0800

    added back to top button

--- testApp.js
+++ testApp.js
@@ -252,6 +252,21 @@ database.ref().on("value", function(snapshot) {
 });
 //******************************************************************************************
 
+$(document).ready(function() {
+  $('a.bktop').click(function(e){
+    $('html, body').animate({scrollTop:0}, '1000');
+    e.preventDefault();
+  });
+  $(window).scroll(function() {
+    if($(this).scrollTop() > 200){
+      $('.bktop').fadeIn('2000');
+    } else {
+      $('.bktop').fadeOut('500');
+    }
+  });
+});
+
+//*******************************************
 
 
 
